45b12ffb6a46c01e1913074062dbcd72c5ce8e2e
mathis kdio <cadio@et.esiea.fr>
2020-11-30 23:46:27 +0100

    Ajout rangée en bas
    Il manque le changement de nombre à droite

--- js/TableButton.js
+++ js/TableButton.js
@@ -39,11 +39,38 @@ function AddColumn() {
     var lastTd = trTbodyChilds[trTbodyChilds.length - 1].querySelectorAll('td');
     lastTd[0].colSpan = thNb - 1;
 }
+
 function AddRow() {
+    var thNb = document.querySelectorAll("th[scope='col']").length;
+    //Ajout <th scope="row">
+    var newtr = document.createElement("tr");
+    var newth = document.createElement("th");
+    newth.scope = 'row';
+    newth.className = 'align-middle';
+    newth.innerText = "0";
+    newtr.appendChild(newth);
+    //Ajout bon nombre de <td><input type="text"></td>
+    for (let index = 0; index < thNb - 2; index++) {
+        var newTd = document.createElement("td");
+        var newInput = document.createElement("input");
+        newInput.type = 'text';
+        newInput.type = 'text';
+        newTd.appendChild(newInput);
+        newtr.appendChild(newTd);
+    }
+
+    var tbodyNodes = document.getElementsByTagName("tbody");
+    var eltLastTr = document.getElementById("lastTr");
+
+    tbodyNodes[0].insertBefore(newtr, eltLastTr);
+
+    //Mise à jour de rowspan pour le dernier tr
     var th = document.querySelectorAll("th[scope='row']");
     thNb = th.length;
-    console.log(thNb);
-
+    var trChilds = tbodyNodes[0].querySelectorAll("tr");
+    var tdChilds = trChilds[0].querySelectorAll("td");
+    var lastTd = tdChilds[tdChilds.length - 1];
+    lastTd.rowSpan = thNb - 1;
 }
    
 var buttonAddColumn  = document.getElementById('button-add-column');
