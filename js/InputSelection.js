6c2faec0e1b409b05b2d828a09fc542398dad2ff
mathis kdio <cadio@et.esiea.fr>
2020-12-02 11:08:54 +0100

    Event est maintenant lors de clic + ctrl enfoncé, fonctionne pour nouvelles cases

--- js/InputSelection.js
+++ js/InputSelection.js
@@ -1,18 +1,12 @@
 function InputSelectBg(cible) {
-    var parent = cible.parentElement;
-    parent.style.backgroundColor = "#17a2b8";
     cible.style.backgroundColor = "#17a2b8";
 }
 
 function InputDeselectBg(cible) {
-    var parent = cible.parentElement;
-    parent.style.backgroundColor = "";
     cible.style.backgroundColor = "";
 }
 
 function SelectMultiple(cible) {
-    const tdInputText = document.getElementsByClassName("tdInputText");
-
     var parent = cible.parentElement;
     parent.style.backgroundColor = "orange";
 }
@@ -26,37 +20,34 @@ function DeselectMultiple() {
 }
 
 let ctrl = 0;
-let mouseDown = 0;
-var cible;
+let clic = 0;
 
 document.addEventListener('keydown', function (event) {
     if (event.key == 'Control') {
         ctrl = 1;
     }
 })
-document.addEventListener('keyup', function () {
-    ctrl = 0;
-})
-document.addEventListener('mousedown', function (event) {
-    if (ctrl == 1) {
-        DeselectMultiple();
-        event.preventDefault();
+
+document.addEventListener('keyup', function (event) {
+    if (event.key == 'Control') {
+        ctrl = 0;
     }
-    mouseDown = 1;
-})
-document.addEventListener('mouseup', function () {
-    mouseDown = 0;
 })
 
-const tdInputText = document.getElementsByClassName("tdInputText");
-
-for (let i = 0; i < tdInputText.length; i++) {
-    tdInputText[i].addEventListener('mouseover', function(event) { 
-        cible = event.target;
-        if (ctrl == 1 && mouseDown == 1) {
-            SelectMultiple(cible);
+function AddEventCtrlClic(tdInputCible) {
+    tdInputCible.addEventListener('click', function (event) {
+        if (ctrl == 1) {
+            clic = 1;
         }
-    });
+        else {
+            clic = 0;
+            DeselectMultiple();
+        }
+
+        if (ctrl == 1 && clic == 1) {
+            SelectMultiple(event.target);
+        }
+    })
 }
 
-export { InputSelectBg, InputDeselectBg };
+export { InputSelectBg, InputDeselectBg, AddEventCtrlClic };
