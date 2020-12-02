fe9e46cfd1fff67f87fb12fbb236861ea0ae6c5a
mathis kdio <cadio@et.esiea.fr>
2020-12-02 10:30:00 +0100

    sélection case par case mais pas les nouvelles

--- js/InputSelection.js
+++ js/InputSelection.js
@@ -1,14 +1,62 @@
 function InputSelectBg(cible) {
-    var parent = cible.parentElement
+    var parent = cible.parentElement;
     parent.style.backgroundColor = "#17a2b8";
     cible.style.backgroundColor = "#17a2b8";
 }
 
 function InputDeselectBg(cible) {
-    var parent = cible.parentElement
+    var parent = cible.parentElement;
     parent.style.backgroundColor = "";
     cible.style.backgroundColor = "";
 }
 
+function SelectMultiple(cible) {
+    const tdInputText = document.getElementsByClassName("tdInputText");
+
+    var parent = cible.parentElement;
+    parent.style.backgroundColor = "orange";
+}
+
+function DeselectMultiple() {
+    const tdInputText = document.getElementsByClassName("tdInputText");
+    for (let i = 0; i < tdInputText.length; i++) {
+        var parent = tdInputText[i].parentElement;
+        parent.style.backgroundColor = ""; 
+    }
+}
+
+let ctrl = 0;
+let mouseDown = 0;
+var cible;
+
+document.addEventListener('keydown', function (event) {
+    if (event.key == 'Control') {
+        ctrl = 1;
+    }
+})
+document.addEventListener('keyup', function () {
+    ctrl = 0;
+})
+document.addEventListener('mousedown', function (event) {
+    if (ctrl == 1) {
+        DeselectMultiple();
+        event.preventDefault();
+    }
+    mouseDown = 1;
+})
+document.addEventListener('mouseup', function () {
+    mouseDown = 0;
+})
+
+const tdInputText = document.getElementsByClassName("tdInputText");
+
+for (let i = 0; i < tdInputText.length; i++) {
+    tdInputText[i].addEventListener('mouseover', function(event) { 
+        cible = event.target;
+        if (ctrl == 1 && mouseDown == 1) {
+            SelectMultiple(cible);
+        }
+    });
+}
 
 export { InputSelectBg, InputDeselectBg };
