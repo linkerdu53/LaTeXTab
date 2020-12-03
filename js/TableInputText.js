ba949d81a6e29a218042f3e4c9988350dcf73041
bastjog <bastienjoguet@gmail.com>
2020-12-03 09:58:39 +0100

    Ajout differentes fonctionnalités bouttons
    italic/centrer/droite/gauche

--- js/TableInputText.js
+++ js/TableInputText.js
@@ -2,6 +2,17 @@ import { casesSelection } from './InputSelection.js';
 
 
 const boldButton = document.getElementById("bold");
+const italicButton = document.getElementById("italic");
+const underlineButton = document.getElementById("underline");
+const textLeftButton = document.getElementById("text-left");
+const textCenterButton = document.getElementById("text-center");
+const textRightButton = document.getElementById("text-right");
+const fontButton = document.getElementById("font");
+const separateButton = document.getElementById("separate");
+const fusionButton = document.getElementById("fusion");
+const infoButton = document.getElementById("info");
+const allButton = document.getElementById("all");
+
 
 boldButton.addEventListener('click', function() {
     for (let i = 0; i < casesSelection.length; i++) {
@@ -13,14 +24,48 @@ boldButton.addEventListener('click', function() {
     }
 });
 
-const boldButton = document.getElementById("italic");
-
-boldButton.addEventListener('click', function() {
+italicButton.addEventListener('click', function() {
     for (let i = 0; i < casesSelection.length; i++) {
-        if (casesSelection[i].style.fontWeight == "bold") {
-            casesSelection[i].style.fontWeight = "normal";
+        if (casesSelection[i].style.fontStyle == "italic") {
+            casesSelection[i].style.fontStyle = "normal";
         } else {
-            casesSelection[i].style.fontWeight = "bold";
+            casesSelection[i].style.fontStyle = "italic";
         }
     }
 });
+
+underlineButton.addEventListener('click', function() {
+    for (let i = 0; i < casesSelection.length; i++) {
+        if (casesSelection[i].style.textDecoration == "underline") {
+            casesSelection[i].style.textDecoration = "inherit";
+        } else {
+            casesSelection[i].style.textDecoration = "underline";
+        }
+    }
+});
+
+
+textLeftButton.addEventListener('click', function() {
+    for (let i = 0; i < casesSelection.length; i++) {
+        casesSelection[i].style.textAlign = "left";
+    }
+});
+
+textCenterButton.addEventListener('click', function() {
+    for (let i = 0; i < casesSelection.length; i++) {
+        casesSelection[i].style.textAlign = "center";
+    }
+});
+
+textRightButton.addEventListener('click', function() {
+    for (let i = 0; i < casesSelection.length; i++) {
+        casesSelection[i].style.textAlign = "right";
+    }
+});
+
+allButton.addEventListener('dblclick', function() {
+    for (let i = 0; i < casesSelection.length; i++) {
+        casesSelection[i].style.textAlign = "right";
+    }
+});
+
