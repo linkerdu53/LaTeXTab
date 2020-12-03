91ad41a32b20f867ec08db54f05c2836e262b7ae
mathis kdio <cadio@et.esiea.fr>
2020-12-03 09:39:36 +0100

    Ajout fonction italic

--- js/TableInputText.js
+++ js/TableInputText.js
@@ -12,3 +12,15 @@ boldButton.addEventListener('click', function() {
         }
     }
 });
+
+const boldButton = document.getElementById("italic");
+
+boldButton.addEventListener('click', function() {
+    for (let i = 0; i < casesSelection.length; i++) {
+        if (casesSelection[i].style.fontWeight == "bold") {
+            casesSelection[i].style.fontWeight = "normal";
+        } else {
+            casesSelection[i].style.fontWeight = "bold";
+        }
+    }
+});
