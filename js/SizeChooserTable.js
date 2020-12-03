cd2eedcd8a3cdce4fcf46c70f5581c83ff108592
mathis kdio <cadio@et.esiea.fr>
2020-12-03 18:17:43 +0100

    Ajout de colonne automatique

--- js/SizeChooserTable.js
+++ js/SizeChooserTable.js
@@ -1,8 +1,12 @@
 const tdSizeChooser = document.getElementsByClassName('tdSizeChooser');
 
 for (let i = 0; i < tdSizeChooser.length; i++) {
-    tdSizeChooser[i].addEventListener('mouseenter', function() {
-        tdSizeChooserMouseEnter(tdSizeChooser[i])
+    tdSizeChooserAddEvent(tdSizeChooser[i]);
+}
+
+function tdSizeChooserAddEvent(cible) {
+    cible.addEventListener('mouseenter', function() {
+        tdSizeChooserMouseEnter(cible)
     });
 }
 
@@ -24,4 +28,32 @@ function tdSizeChooserMouseEnter(cible) {
     tdSizeChooserResetBackground();
 
     tdSizeChooserSetBackground(cible.dataset.row, cible.dataset.col);
+    let rowSize = tdSizeChooser[tdSizeChooser.length - 1].dataset.row;
+    let columnSize = tdSizeChooser[tdSizeChooser.length - 1].dataset.col;
+    if(cible.dataset.col == columnSize) {
+        tdSizeChooserAddColumn(columnSize);
+    }
+    if(cible.dataset.row == rowSize) {
+        tdSizeChooserAddRow(columnSize);
+    }
+}
+
+function tdSizeChooserAddColumn(columnSize) {
+
+    let tdLastCol = document.querySelectorAll("[data-col='"+columnSize+"']")
+    console.log(tdLastCol);
+    for (let i = 0; i < tdLastCol.length; i++) {
+        const newTd = document.createElement("td");
+        newTd.classList.add("tdSizeChooser");
+        newTd.dataset.row = i + 1;
+        newTd.dataset.col = parseInt(columnSize) + 1;
+        tdSizeChooserAddEvent(newTd);
+
+        let trParent = tdLastCol[i].parentElement;
+        trParent.appendChild(newTd);
+    }
+}
+
+function tdSizeChooserAddRow() {
+
 }
