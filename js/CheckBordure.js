e419824ad52ee730661237589b0c8485eabb0635
bastjog <bastienjoguet@gmail.com>
2021-03-03 14:30:29 +0100

    Update CheckBordure.js

--- js/CheckBordure.js
+++ js/CheckBordure.js
@@ -3,23 +3,14 @@ import { GenerateToLatex } from './GenerateToLatex.js';
 var tdInputText = document.getElementsByClassName("tdInputText");
 
 function CheckBordureRight() {
-    //console.log(tdInputText.length);
-    console.log(tdInputText[0].classList);
-    if (tdInputText[0].classList.contains("borderLeftOn"))
+    for (let i = 0; i < tdInputText.length; i++)
     {
-        console.log("oui oui oui"); 
-    }
-    for (let i = 0; i < tdInputText.lentgh; i++)
-    {
-        console.log(tdInputText[i].classList);
-        console.log("2");
-        /*if (tdInputText[i].classList.contains("borderLeftOn"))
+        if (tdInputText[i].classList.contains("borderLeftOn"))
         {
             tdInputText[i].style.borderLeft = "";
             tdInputText[i].classList.remove(borderLeftOn);
-        }*/
+        }
     }
-    console.log("3");
 }
 
 
