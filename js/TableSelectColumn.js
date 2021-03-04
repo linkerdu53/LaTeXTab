6b967f6b8f7e2fe5fc15e0bc7ad09dfb57fdc41a
mathis kdio <cadio@et.esiea.fr>
2021-03-04 12:42:35 +0100

    Fonction d'ajout event

--- js/TableSelectColumn.js
+++ js/TableSelectColumn.js
@@ -3,5 +3,11 @@ import { SelectColumn } from './InputSelection.js';
 const columnButton = document.querySelectorAll("th[scope='col']");
 //i = 1 car columnButton[0] est l'angle haut gauche du tableau et length - 1 car angle haut droit ne sont pas colonnes d'inputs
 for (let i = 1; i < columnButton.length - 1; i++) {
-    columnButton[i].addEventListener('click', function() { SelectColumn(i) })
+    AddEventSelectColumn(columnButton[i], i);
 }
+
+function AddEventSelectColumn(target, columnId) {
+    target.addEventListener('click', function() { SelectColumn(columnId) });
+}
+
+export { AddEventSelectColumn }
--- js/TableSelectRow.js
+++ js/TableSelectRow.js
@@ -3,5 +3,11 @@ import { SelectRow } from './InputSelection.js';
 const rowButton = document.querySelectorAll("th[scope='row']");
 //length - 1 car angle bas gauche n'est pas une rangée d'inputs
 for (let i = 0; i < rowButton.length - 1; i++) {
-    rowButton[i].addEventListener('click', function() { SelectRow(i + 1) })
+    AddEventSelectRow(rowButton[i], i + 1);
 }
+
+function AddEventSelectRow(target, rowId) {
+    target.addEventListener('click', function() { SelectRow( rowId ) });
+}
+
+export { AddEventSelectRow }
