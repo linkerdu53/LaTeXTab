02b3c063063e668c0106ffeb10f54a229fd1d8a6
mathis kdio <cadio@et.esiea.fr>
2021-03-22 20:13:12 +0100

    fix

--- js/CheckBordure.js
+++ js/CheckBordure.js
@@ -3,71 +3,83 @@ import { casesSelection } from './InputSelection.js';
 
 const tdInputText = document.getElementsByClassName("tdInputText");
 
+//Check la bordure à gauche de la case à droite de la case tdInputText[i]
 function CheckBordureRight(input) {
     let nbCol = parseInt(tdInputText[tdInputText.length - 1].parentNode.dataset.col);
     if (parseInt(input.parentNode.dataset.col) !== nbCol) {
         let inputDroit = tdInputText[(parseInt(input.parentNode.dataset.row) - 1) * nbCol + (parseInt(input.parentNode.dataset.col) + 1) - 1];
-        if (!input.classList.contains("borderRightOn")) {
-            if (inputDroit.classList.contains("borderLeftOn") && !inputDroit.classList.contains("selected")) {
-                inputDroit.style.borderLeft = "";
-                inputDroit.classList.remove("borderLeftOn");
+        if (!inputDroit.classList.contains("selected")) {
+            if (!input.classList.contains("borderRightOn")) {
+                if (inputDroit.classList.contains("borderLeftOn")) {
+                    inputDroit.style.borderLeft = "";
+                    inputDroit.classList.remove("borderLeftOn");
+                }
             }
-        }
-        else if (!inputDroit.classList.contains("borderLeftOn")) {
-            inputDroit.style.borderLeft = "solid";
-            inputDroit.classList.add("borderLeftOn");
+            else if (!inputDroit.classList.contains("borderLeftOn")) {
+                inputDroit.style.borderLeft = "solid";
+                inputDroit.classList.add("borderLeftOn");
+            }
         }
     }
 }
 
+//Check la bordure droite de la case à gauche de la case tdInputText[i]
 function CheckBordureLeft(input) {
     let nbCol = parseInt(tdInputText[tdInputText.length - 1].parentNode.dataset.col);
     if (parseInt(input.parentNode.dataset.col) !== 1) {
         let inputGauche = tdInputText[(parseInt(input.parentNode.dataset.row) - 1) * nbCol + (parseInt(input.parentNode.dataset.col) -1) - 1];
-        if (!input.classList.contains("borderLeftOn")) {
-            if (inputGauche.classList.contains("borderRightOn") && !inputGauche.classList.contains("selected")) {             
-                inputGauche.style.borderRight = "";
-                inputGauche.classList.remove("borderRightOn");
+        if (!inputGauche.classList.contains("selected")) {
+            if (!input.classList.contains("borderLeftOn")) {
+                if (inputGauche.classList.contains("borderRightOn")) {             
+                    inputGauche.style.borderRight = "";
+                    inputGauche.classList.remove("borderRightOn");
+                }
             }
-        }
-        else if (!inputGauche.classList.contains("borderRightOn")) {
-            inputGauche.style.borderRight = "solid";
-            inputGauche.classList.add("borderRightOn");
+            else if (!inputGauche.classList.contains("borderRightOn")) {
+                inputGauche.style.borderRight = "solid";
+                inputGauche.classList.add("borderRightOn");
+            }
         }
     }
 }
 
+//Check la bordure du bas de la case au dessus de la case tdInputText[i]
 function CheckBordureTop(input) {
     let nbCol = parseInt(tdInputText[tdInputText.length - 1].parentNode.dataset.col);
     if (parseInt(input.parentNode.dataset.row) !== 1) {
         let inputDessus = tdInputText[(parseInt(input.parentNode.dataset.row) - 2) * nbCol + parseInt(input.parentNode.dataset.col) - 1];
-        if (!input.classList.contains("borderTopOn")) {
-            if (inputDessus.classList.contains("borderBottomOn") && !inputDessus.classList.contains("selected")) {             
-                inputDessus.style.borderBottom = "";
-                inputDessus.classList.remove("borderBottomOn");
+        if (!inputDessus.classList.contains("selected")) {
+            if (!input.classList.contains("borderTopOn")) {
+                if (inputDessus.classList.contains("borderBottomOn")) {
+                    inputDessus.style.borderBottom = "";
+                    inputDessus.classList.remove("borderBottomOn");
+                }
             }
-        }
-        else if (!inputDessus.classList.contains("borderBottomOn")) {
-            inputDessus.style.borderBottom = "solid";
-            inputDessus.classList.add("borderBottomOn");
+            else if (!inputDessus.classList.contains("borderBottomOn")) {
+                inputDessus.style.borderBottom = "solid";
+                inputDessus.classList.add("borderBottomOn");
+            }
         }
     }
 }
 
+//Check la bordure du haut de la case en dessous de la case tdInputText[i]
 function CheckBordureBottom(input) {
     let nbCol = parseInt(tdInputText[tdInputText.length - 1].parentNode.dataset.col);
     let nbRow = parseInt(tdInputText[tdInputText.length - 1].parentNode.dataset.row);
     if (parseInt(input.parentNode.dataset.row) !== nbRow) {
         let inputDessous = tdInputText[parseInt(input.parentNode.dataset.row) * nbCol + parseInt(input.parentNode.dataset.col) - 1];
-        if (!input.classList.contains("borderBottomOn")) {
-            if (inputDessous.classList.contains("borderTopOn") && !inputDessous.classList.contains("selected")) {
-                inputDessous.style.borderTop = "";
-                inputDessous.classList.remove("borderTopOn");
+        if (!inputDessous.classList.contains("selected")) {
+            if (!input.classList.contains("borderBottomOn")) {
+                if (inputDessous.classList.contains("borderTopOn")) {
+                    inputDessous.style.borderTop = "";
+                    inputDessous.classList.remove("borderTopOn");
+                }
             }
-        }
-        else if (!inputDessous.classList.contains("borderTopOn")) {
-            inputDessous.style.borderTop = "solid";
-            inputDessous.classList.add("borderTopOn");
+            else if (!inputDessous.classList.contains("borderTopOn")) {
+                inputDessous.style.borderTop = "solid";
+                inputDessous.classList.add("borderTopOn");
+            }
         }
     }
 }
