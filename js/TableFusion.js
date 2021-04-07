bb5853a039299c08e9af1533c08b1ac911cf9510
mathis kdio <cadio@et.esiea.fr>
2021-04-07 23:33:06 +0200

    Update TableFusion.js

--- js/TableFusion.js
+++ js/TableFusion.js
@@ -1,6 +1,15 @@
 import { casesSelection } from './InputSelection.js';
 import { AddEventInput } from './TableInput.js';
 
+//Test si les 2 strings contiennent au moins 1 nombre en commun
+function IsStrsContains1Elt(row1, row2) {
+    if (row1.split(' ').some(v => row2.includes(v))) {
+        return true
+    } else {
+        return false
+    }
+}
+
 function Fusion() {
     // Pas assez de cases à fusionner
     if (casesSelection.length < 2) {
@@ -12,13 +21,19 @@ function Fusion() {
     casesSort.sort((a, b) => { return a.parentNode.dataset.row - b.parentNode.dataset.row })
     let tdGroups = []
     tdGroups.push([casesSort[0].parentNode])
+    console.log(casesSort)
+    //On parcours de gauche à droite et de haut en bas
     for (let i = 1; i < casesSort.length; i++) {
         //Test si sur la même ligne et à coté du groupe précédent
-        if (casesSort[i].parentNode.dataset.row == tdGroups[tdGroups.length - 1][tdGroups[tdGroups.length - 1].length - 1].dataset.row && casesSort[i].parentNode.dataset.col - 1 == tdGroups[tdGroups.length - 1][tdGroups[tdGroups.length - 1].length - 1].dataset.col) {
+        let caseRow = casesSort[i].parentNode.dataset.row
+        let caseCol = casesSort[i].parentNode.dataset.col
+        let lastGroupRow = tdGroups[tdGroups.length - 1][tdGroups[tdGroups.length - 1].length - 1].dataset.row
+        let lastGroupCol = tdGroups[tdGroups.length - 1][tdGroups[tdGroups.length - 1].length - 1].dataset.col
+        if (IsStrsContains1Elt(caseRow, lastGroupRow)) {
             tdGroups[tdGroups.length - 1].push(casesSort[i].parentNode)
         }
         //Test si sur la même colonne et en dessous
-        else if (casesSort[i].parentNode.dataset.row - 1 == tdGroups[tdGroups.length - 1][tdGroups[tdGroups.length - 1].length - 1].dataset.row && casesSort[i].parentNode.dataset.col == tdGroups[tdGroups.length - 1][tdGroups[tdGroups.length - 1].length - 1].dataset.col) {
+        else if (IsStrsContains1Elt(caseCol, lastGroupCol)) {
             tdGroups[tdGroups.length - 1].push(casesSort[i].parentNode)
         }
         //Sinon on créer une nouvelle zone
@@ -27,14 +42,21 @@ function Fusion() {
         }
     }
 
+    console.log(tdGroups)
+
     //Une fois les cases regroupée par zones
     for (let i = 0; i < tdGroups.length; i++) {
         if (tdGroups[i].length > 1) { // Il faut + d'une case pour fusionner
             const newTd = document.createElement("td")
-            newTd.dataset.row = tdGroups[i][0].dataset.row
-            newTd.dataset.col = tdGroups[i][0].dataset.col
-            if (tdGroups[i][0].dataset.row == tdGroups[i][1].dataset.row) { //Si sur la même ligne
-                newTd.colSpan = tdGroups[i].length
+
+            //On stock le premier et le dernier
+            newTd.dataset.row = tdGroups[i][0].dataset.row + ' ' + tdGroups[i][tdGroups[i].length - 1].dataset.row
+            newTd.dataset.col = tdGroups[i][0].dataset.col + ' ' + tdGroups[i][tdGroups[i].length - 1].dataset.col
+            if (IsStrsContains1Elt(tdGroups[i][0].dataset.row, tdGroups[i][1].dataset.row)) {
+                for (let j = 0; j < tdGroups[i].length; j++) {
+                    newTd.colSpan += tdGroups[i][j].dataset.col.split(" ").length
+                }
+                newTd.colSpan-- //Car colSpan init à 1
             }
             else {
                 newTd.rowSpan = tdGroups[i].length
@@ -43,10 +65,18 @@ function Fusion() {
             const newInput = document.createElement("input")
             newInput.type = 'text'
             newInput.classList.add("tdInputText");
-            if (tdGroups[i][0].dataset.row == tdGroups[i][1].dataset.row) { //Si sur la même ligne
+            if (IsStrsContains1Elt(tdGroups[i][0].dataset.row, tdGroups[i][1].dataset.row)) { //Si sur la même ligne
                 let newWidth = 0
                 for (let j = 0; j < tdGroups[i].length; j++) {
-                    newWidth += parseInt(tdGroups[i][j].children[0].style.width)
+                    if (tdGroups[i][j].children[0].style.width) {
+                        newWidth += parseInt(tdGroups[i][j].children[0].style.width) + 7
+                    }
+                    else if (tdGroups[i][j].children[0].style.minWidth) {
+                        newWidth += parseInt(tdGroups[i][j].children[0].style.minWidth) + 7
+                    }
+                    else {
+                        newWidth += 40
+                    }
                 }
                 newInput.style.minWidth = newWidth + 'px'
             }
@@ -57,7 +87,7 @@ function Fusion() {
                         newHeight += parseInt(tdGroups[i][j].children[0].style.lineHeight)
                     }
                     else {
-                        newHeight += 32
+                        newHeight += 40
                     }
                 }
                 newInput.style.lineHeight = newHeight + 'px'
