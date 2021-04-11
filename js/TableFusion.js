aa07a769fcbf5895ee27be86d6ccf303a425c6cc
mathis kdio <cadio@et.esiea.fr>
2021-04-11 11:25:29 +0200

    Changement algo répartition des groupes de cases à fusionner

--- js/TableFusion.js
+++ js/TableFusion.js
@@ -1,9 +1,11 @@
-import { casesSelection } from './InputSelection.js';
+import { casesSelection, DeselectAllInput } from './InputSelection.js';
 import { AddEventInput } from './TableInput.js';
 
+const tdInputText = document.getElementsByClassName('tdInputText');
+
 //Test si les 2 strings contiennent au moins 1 nombre en commun
-function IsStrsContains1Elt(row1, row2) {
-    if (row1.split(' ').some(v => row2.includes(v))) {
+function IsStrsContains1Elt(str1, str2) {
+    if (str1.split(' ').some(v => str2.includes(v))) {
         return true
     } else {
         return false
@@ -17,32 +19,85 @@ function Fusion() {
     }
     // Regroupage des cases à fusioner
     let casesSort = casesSelection.slice()
-    casesSort.sort((a, b) => { return a.parentNode.dataset.col - b.parentNode.dataset.col })
-    casesSort.sort((a, b) => { return a.parentNode.dataset.row - b.parentNode.dataset.row })
-    let tdGroups = []
-    tdGroups.push([casesSort[0].parentNode])
-    console.log(casesSort)
-    //On parcours de gauche à droite et de haut en bas
-    for (let i = 1; i < casesSort.length; i++) {
-        //Test si sur la même ligne et à coté du groupe précédent
-        let caseRow = casesSort[i].parentNode.dataset.row
-        let caseCol = casesSort[i].parentNode.dataset.col
-        let lastGroupRow = tdGroups[tdGroups.length - 1][tdGroups[tdGroups.length - 1].length - 1].dataset.row
-        let lastGroupCol = tdGroups[tdGroups.length - 1][tdGroups[tdGroups.length - 1].length - 1].dataset.col
-        if (IsStrsContains1Elt(caseRow, lastGroupRow)) {
-            tdGroups[tdGroups.length - 1].push(casesSort[i].parentNode)
-        }
-        //Test si sur la même colonne et en dessous
-        else if (IsStrsContains1Elt(caseCol, lastGroupCol)) {
-            tdGroups[tdGroups.length - 1].push(casesSort[i].parentNode)
-        }
-        //Sinon on créer une nouvelle zone
-        else {
-            tdGroups.push([casesSort[i].parentNode])
-        }
-    }
+    casesSort.sort((a, b) => { return a.parentNode.dataset.col.split(' ')[0] - b.parentNode.dataset.col.split(' ')[0] })
+    casesSort.sort((a, b) => { return a.parentNode.dataset.row.split(' ')[0] - b.parentNode.dataset.row.split(' ')[0] })
 
-    console.log(tdGroups)
+    let tdGroups = []
+    //Parcours de gauche à droite et de haut en bas
+    for (let i = 0; i < casesSort.length; i++) {
+        //Test si case est sélectionnée
+        let voisinCote = 0 // 0 = pas de voisin sel, 1 = bas et 2 = droite
+        //Si c'est le cas alors on créer un groupe puis on check les voisins pour fusionner
+        tdGroups.push([casesSort[i].parentNode])
+        //Comme sur Excel si le voisin en-dessous ou à droite est sélectionné alors on fusionnera en-dessous
+        //Voisin en-dessous
+        let casePrecedente = casesSort[i].parentNode;
+        let newRowNumber = casesSort[i].parentNode.dataset.row.split(" ")
+        newRowNumber = newRowNumber[newRowNumber.length - 1]
+        newRowNumber = (parseInt(newRowNumber) + 1).toString()
+        for (let j = 0; j < tdInputText.length; j++) {
+            if (IsStrsContains1Elt(tdInputText[j].parentNode.dataset.row, newRowNumber) && IsStrsContains1Elt(tdInputText[j].parentNode.dataset.col, casesSort[i].parentNode.dataset.col)) {
+                if (casesSelection.includes(tdInputText[j]) && tdInputText[j].parentNode.dataset.col == casesSort[i].parentNode.dataset.col) {
+                    voisinCote = 1
+                }
+                break;
+            }
+        }
+        //Si le voisin en-dessous n'est pas sélectionné alors on regarde celui de droite
+        if (voisinCote == 0) {
+            //Voisin à droite
+            let newColNumber = casesSort[i].parentNode.dataset.col.split(" ")
+            newColNumber = newColNumber[newColNumber.length - 1]
+            newColNumber = (parseInt(newColNumber) + 1).toString()
+            for (let j = 0; j < tdInputText.length; j++) {
+                if (IsStrsContains1Elt(tdInputText[j].parentNode.dataset.row, casesSort[i].parentNode.dataset.row) && IsStrsContains1Elt(tdInputText[j].parentNode.dataset.col, newColNumber)) {
+                    if (casesSelection.includes(tdInputText[j]) && tdInputText[j].parentNode.dataset.row == casesSort[i].parentNode.dataset.row) {
+                        voisinCote = 2
+                    }
+                    break;
+                }
+            }
+        }
+        //Si voisin trouvé alors on continue à regarder sur la ligne ou la colonne tant les cases sélectionnée
+        if (voisinCote != 0) {
+            for (let j = 0; j < tdInputText.length; j++) {
+                if (voisinCote == 1) { //En dessous
+                    //Si case sur même colonne et ligne juste en dessous
+                    let newRowNumber = casePrecedente.dataset.row.split(" ")
+                    newRowNumber = newRowNumber[newRowNumber.length - 1]
+                    newRowNumber = (parseInt(newRowNumber) + 1).toString()
+                    if (IsStrsContains1Elt(tdInputText[j].parentNode.dataset.row, newRowNumber) && IsStrsContains1Elt(tdInputText[j].parentNode.dataset.col, casesSort[i].parentNode.dataset.col)) {
+                        if (casesSelection.includes(tdInputText[j]) && tdGroups[tdGroups.length - 1].includes(tdInputText[j].parentNode) == false) { //Si selectionnee
+                            casePrecedente = tdInputText[j].parentNode
+                            tdGroups[tdGroups.length - 1].push(tdInputText[j].parentNode)
+                            casesSort.splice(casesSort.indexOf(tdInputText[j]), 1)
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                }
+                else { //A droite
+                    //Si case sur même ligne et colonne juste à côté
+                    let newColNumber = casePrecedente.dataset.col.split(" ")
+                    newColNumber = newColNumber[newColNumber.length - 1]
+                    newColNumber = (parseInt(newColNumber) + 1).toString()
+                    if (IsStrsContains1Elt(tdInputText[j].parentNode.dataset.row, casesSort[i].parentNode.dataset.row) && IsStrsContains1Elt(tdInputText[j].parentNode.dataset.col, newColNumber)) {
+                        if (casesSelection.includes(tdInputText[j]) && tdGroups[tdGroups.length - 1].includes(tdInputText[j].parentNode) == false) { //Si selectionnee
+                            casePrecedente = tdInputText[j].parentNode
+                            tdGroups[tdGroups.length - 1].push(tdInputText[j].parentNode)
+                            casesSort.splice(casesSort.indexOf(tdInputText[j]), 1)
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+        //Sinon on passe à la case sélectionnée suivante
+    }
 
     //Une fois les cases regroupée par zones
     for (let i = 0; i < tdGroups.length; i++) {
@@ -50,15 +105,34 @@ function Fusion() {
             const newTd = document.createElement("td")
 
             //On stock le premier et le dernier
-            newTd.dataset.row = tdGroups[i][0].dataset.row + ' ' + tdGroups[i][tdGroups[i].length - 1].dataset.row
-            newTd.dataset.col = tdGroups[i][0].dataset.col + ' ' + tdGroups[i][tdGroups[i].length - 1].dataset.col
-            if (IsStrsContains1Elt(tdGroups[i][0].dataset.row, tdGroups[i][1].dataset.row)) {
-                for (let j = 0; j < tdGroups[i].length; j++) {
-                    newTd.colSpan += tdGroups[i][j].dataset.col.split(" ").length
+            let dataRow = []
+            let dataCol = []
+            for (let j = 0; j < tdGroups[i].length; j++) {
+                if (dataRow.includes(tdGroups[i][j].dataset.row) == false) {
+                    dataRow.push(tdGroups[i][j].dataset.row)
                 }
-                newTd.colSpan-- //Car colSpan init à 1
+                if (dataCol.includes(tdGroups[i][j].dataset.col) == false) {
+                    dataCol.push(tdGroups[i][j].dataset.col)
+                }
+            }
+            newTd.dataset.row = dataRow.join(" ")
+            newTd.dataset.col = dataCol.join(" ")
+
+            if (IsStrsContains1Elt(tdGroups[i][0].dataset.row, tdGroups[i][1].dataset.row)) {
+                let newColSpan = 0
+                let newRowSpan = 0
+                for (let j = 0; j < tdGroups[i].length; j++) {
+                    newColSpan += tdGroups[i][j].dataset.col.split(" ").length
+                }
+                newTd.colSpan = newColSpan
+                for (let j = 0; j < tdGroups[i].length; j++) {
+                    newRowSpan += tdGroups[i][j].dataset.col.split(" ").length
+                }
+                newRowSpan = tdGroups[i][0].rowSpan
+                newTd.rowSpan = newRowSpan
             }
             else {
+                newTd.colSpan = tdGroups[i][0].colSpan
                 newTd.rowSpan = tdGroups[i].length
             }
             newTd.classList.add("tdMainTable")
@@ -79,8 +153,11 @@ function Fusion() {
                     }
                 }
                 newInput.style.minWidth = newWidth + 'px'
+                if (tdGroups[i][0].children[0].style.lineHeight) {
+                    newInput.style.lineHeight = tdGroups[i][0].children[0].style.lineHeight
+                }
             }
-            else {
+            else { //Si sur la même colonne
                 let newHeight = 0
                 for (let j = 0; j < tdGroups[i].length; j++) {
                     if (tdGroups[i][j].children[0].style.lineHeight) {
@@ -91,6 +168,9 @@ function Fusion() {
                     }
                 }
                 newInput.style.lineHeight = newHeight + 'px'
+                if (tdGroups[i][0].children[0].style.minWidth) {
+                    newInput.style.minWidth = tdGroups[i][0].children[0].style.minWidth
+                }
             }
             AddEventInput(newInput)
 
@@ -103,6 +183,8 @@ function Fusion() {
             }
         }
     }
+    //Toutes les cases sélectionnées sont retirées
+    DeselectAllInput()
 }
 
 export { Fusion }
