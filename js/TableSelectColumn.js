a6640f905fd1b1f5dc8b530d7a723aee7be26a7d
bastjog <bastienjoguet@gmail.com>
2021-05-07 17:21:42 +0200

    Update TableSelectColumn.js

--- js/TableSelectColumn.js
+++ js/TableSelectColumn.js
@@ -1,17 +1,22 @@
 import { SelectColumn } from './InputSelection.js';
+import { AddColumn, SupprColumn } from './TableButton.js';
 
 const columnButton = document.querySelectorAll("th[scope='col']");
 //i = 1 car columnButton[0] est l'angle haut gauche du tableau et length - 1 car angle haut droit ne sont pas colonnes d'inputs
 for (let i = 1; i < columnButton.length - 1; i++) {
     AddEventSelectColumn(columnButton[i], i);
+    addColumnIn(columnButton[i], i);
 }
 
 function AddEventSelectColumn(target, columnId) {
     target.addEventListener('click', function() { SelectColumn(columnId) });
 }
 
-function addColumnIn() {
- 
+function addColumnIn(target, columnId) {
+    target.addEventListener('click', function() { 
+        AddColumn();
+        console.log(columnId);
+    });
 
 }
 
