d5c5dfcb28f43613bda4a34cd3e0ca13584b1878
mathis kdio <cadio@et.esiea.fr>
2021-05-18 14:33:00 +0200

    Fix mauvaises données dans data-col après la séparation de cases fusionnées (#63)

--- js/TableSplit.js
+++ js/TableSplit.js
@@ -16,18 +16,16 @@ function Split() {
                 //On récupère les lignes du tableau, à chaque itération on passe à la suivante
                 let ligneInsert = document.getElementsByClassName('mainTable')[0].querySelectorAll("tr")[dataRow[j]]
                 //Pour chaque ligne, on récupère la case adjacent droite pour insérer la/les case(s) avant
-                console.log(ligneInsert)
-                let eltBeforeInsert
-                if (j == 0) {
-                    eltBeforeInsert = ligneInsert.children[dataCol[0]].nextElementSibling
+                let eltBeforeInsert = null
+                //On cherche le td que l'on va utiliser pour insérer avant celui-ci
+                //On parcours les cases de la même ligne à la recherche de celle qui contient la colonne suivante de la case fusionnée
+                for (let jcol = 0; jcol < tableSize.col; jcol++) {
+                    let inputCol = tableMatrice[dataRow[j] - 1][jcol].parentElement.dataset.col.split(" ").map(Number)
+                    if (inputCol.includes(dataCol[dataCol.length - 1] + 1)) {
+                        eltBeforeInsert = tableMatrice[dataRow[j] - 1][jcol].parentElement
+                        break
+                    }
                 }
-                else {
-                    eltBeforeInsert = ligneInsert.children[dataCol[0]]
-                }
-                console.log(dataCol[0])
-                console.log(ligneInsert.children)
-                console.log(ligneInsert.children[dataCol[0]])
-                console.log(eltBeforeInsert)
                 for (let k = 0; k < dataCol.length; k++) {
                     const newInput = document.createElement("input")
                     newInput.type = 'text'
