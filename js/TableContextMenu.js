a094f1dc7c16172a350993bad49eccdea135928e
mathis kdio <cadio@et.esiea.fr>
2021-06-07 11:47:14 +0200

    Update TableContextMenu.js
    Possible d'ajouter une ligne avant ou après
    détection si clic pour ajout ligne ou colonne

--- js/TableContextMenu.js
+++ js/TableContextMenu.js
@@ -1,43 +1,52 @@
 //FROM https://dgoguerra.github.io/bootstrap-menu/demos.html
-import { AddColumn } from './TableButton.js'; 
-let demo5Rows = {
-    '1': { first_name: 'John', last_name: 'Martin', is_editable: true },
-    '2': { first_name: 'Peter', last_name: 'Roberts', is_editable: true },
-    '3': { first_name: 'Stuart', last_name: 'Smith', is_editable: false }
-};
-  
+import { AddColumn, AddRow } from './TableButton.js'
+
 let menu = new BootstrapMenu('.contextMenu', {
     fetchElementData: function($rowElem) {
-        //var rowId = $rowElem.data('rowId');
-        let rowId = 1;
-        return demo5Rows[rowId];
+        let id
+        let type
+        //Test si clic sur ligne
+        if ($rowElem[0].parentElement.parentElement.nodeName == "TBODY") {
+            type = 'row'
+            let trList = document.getElementById("bodyMainTable").children
+            id = Array.from(trList).indexOf($rowElem[0].parentElement)
+        }
+        else {
+            type = 'col'
+            id = 0
+        }
+        return [type, id];
     },
     actions: [
         {
-            name: function(row) {
-                // custom action name, with the name of the selected user
-                if (row.is_editable) {
-                    return 'Ajouter une colonne';
-                }
-                // basic HTML can also be rendered in the action name
-                return '<i>Not editable</i>';
-            },
+            name: 'Ajouter une colonne/ligne avant',
             iconClass: 'bi bi-pencil-fill',
-            classNames: function(row) {
-                // add the 'action-success' class only if the row is editable
-                return { 'action-success': row.is_editable };
-            },
-            onClick: function(row) {
-                AddColumn();
-            },
-            isEnabled: function(row) {
-                return row.is_editable;
+            classNames: 'action-success',
+            onClick: function(data) {
+                if (data[0] == 'row') {
+                    AddRow(data[1])
+                }
+                else {
+                    AddColumn(data[1])
+                }
             }
         },
         {
-            name: 'Supprimer une colonne',
+            name: 'Ajouter une colonne/ligne après',
+            iconClass: 'bi bi-pencil-fill',
+            classNames: 'action-success',
+            onClick: function(data) {
+                if (data[0] == 'row') {
+                    AddRow(data[1] + 1)
+                }
+                else {
+                    AddColumn(data[1] + 1)
+                }
+            }
+        },
+        {
+            name: 'Supprimer la colonne/ligne',
             iconClass: 'bi bi-trash',
-            // always add the class 'action-danger'
             classNames: 'action-danger',
             onClick: function(row) {
                 alert("test");
